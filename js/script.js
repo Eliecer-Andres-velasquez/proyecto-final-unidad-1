57f60163b7982b31eb3e454ad78ed26d141339fc
Eliecer-Andres-Velasquez <elias.v.ariasvea@gmail.com>
2021-07-24 17:25:57 -0500

    actividad final

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function confirmacion() {
+    return confirm("Quieres ingresar los datos para participar en la rifa del movil")
+    
+}
+
+var nombreSitio =prompt("¿Qué sabes de los grupos vulnerables?")
+alert("Son persona o grupo que por sus características de desventaja por edad, sexo, estado civil; nivel educativo, origen étnico, situación o condición física y/o mental; requieren de un esfuerzo adicional para incorporarse al desarrollo y a la convivencia");
+
+  
+
+
+
+
+
